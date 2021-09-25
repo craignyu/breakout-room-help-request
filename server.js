0e58993aa0c7561f7a80e3a47f1a9ce376594a92
craignyu <kapp@cs.nyu.edu>
2021-09-25 12:53:19 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const express = require('express');
+const socketIO = require('socket.io');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(server);
+
+// persistent room data
+let rooms = {};
+for (let i = 1; i <= 40; i++) {
+  rooms[`room${(''+i).padStart(2, '0')}`] = false;
+}
+console.log(rooms);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+
+  // immediately send the client the current room status
+  let jsonRooms = JSON.stringify(rooms);
+  socket.emit('roomupdate', jsonRooms);
+
+  socket.on('disconnect', () => console.log('Client disconnected'));
+  socket.on('roomchange', function(data) {
+    console.log("room change:", data);
+    rooms[data] = !rooms[data];
+    let jsonRooms = JSON.stringify(rooms);
+    io.emit('roomupdate', jsonRooms);
+  });
+});
+
+/*
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+*/
